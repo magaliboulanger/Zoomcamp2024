46ce32171b082f01be876917e298dee2936095fc
magaliboulanger <56554227+magaliboulanger@users.noreply.github.com>
2024-02-02 10:10:44 -0300

    Update main.tf

--- terrademo/basic/main.tf
+++ terrademo/basic/main.tf
@@ -10,14 +10,14 @@ terraform {
 provider "google" {
   # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
   #  credentials = 
-  project = "<Your Project ID>"
+  project = "grounded-primer-411515"
   region  = "us-central1"
 }
 
 
 
 resource "google_storage_bucket" "data-lake-bucket" {
-  name     = "<Your Unique Bucket Name>"
+  name     = "zoomcamp-bucket-mboulanger"
   location = "US"
 
   # Optional, but recommended settings:
@@ -42,7 +42,7 @@ resource "google_storage_bucket" "data-lake-bucket" {
 
 
 resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "<The Dataset Name You Want to Use>"
-  project    = "<Your Project ID>"
+  dataset_id = "yellow_taxi_trips"
+  project    = "grounded-primer-411515"
   location   = "US"
 }
